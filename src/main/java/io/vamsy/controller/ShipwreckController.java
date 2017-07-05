f9345c2ca5ade756b8d38d2797a77ebf5e2fcc03
vamsyramk <vamsyramk@gmail.com>
2017-07-05 11:24:34 -0400

    third commit

--- src/main/java/io/vamsy/controller/ShipwreckController.java
+++ src/main/java/io/vamsy/controller/ShipwreckController.java
@@ -1,7 +1,13 @@
 package io.vamsy.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by vamsyramk on 7/5/2017.
  */
+@RestController
+@RequestMapping("api/v1/")
 public class ShipwreckController {
+
 }
